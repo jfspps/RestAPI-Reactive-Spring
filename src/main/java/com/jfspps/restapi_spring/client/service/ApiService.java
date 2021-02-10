86cebc00a6de21b7eaf8c597174ea80ecb4fe598
jfspps <jfsapps@gmail.com>
2021-02-10 00:44:51 +0000

    edited non-reactive but still there are build problems

--- src/main/java/com/jfspps/restapi_spring/client/service/ApiService.java
+++ src/main/java/com/jfspps/restapi_spring/client/service/ApiService.java
@@ -1,10 +1,14 @@
 package com.jfspps.restapi_spring.client.service;
 
 import com.jfspps.restapi_spring.api.model.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
 
     List<User> getUsers(Integer limit);
+
+    Flux<User> getUsers(Mono<Integer> limit);
 }
